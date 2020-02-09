1f4ebcc5fa229cd8553791c08298e4718a6d7a40
Piyush Raj <piyush.189303055@muj.manipal.edu>
2020-02-09 14:50:07 +0530

    Jira getversion() method implemented

--- README.md
+++ README.md
@@ -2,9 +2,8 @@
 
 <p align="center">
 <b>Jiraffe - One stop place for exploiting all Jira instances in your proximity</b><br><br>
-    <img alt="Jiraffe logo" src="static/logo.png" width="400"><br>
-<b>See the <a href="#Usage">demo</a>.<br>
-  See how it <a href="#How">works</a>.<br><br></b>
+<img alt="Jiraffe logo" src="static/logo.png" width="400"><br>
+See the <a href="#demo">demo</a>
 </p>
 
 
@@ -17,3 +16,22 @@
 |  CVE-2019-11581  |  VERY HIGH  |  Atlassian JIRA Template injection vulnerability RCE  |  Jira < 7.6.14  |  [CVE-2019-11581](https://lmgtfy.com/?q=CVE-2019-11581)  |
 |  CVE-2019-8451  |  HIGH  |  Pre-authentication server side request forgery (SSRF) vulnerability found in the /plugins/servlet/gadgets/makeRequest resource.  |  Jira == 7.6.0  && Jira.7.6.0 < 7.13.9, 8.4.0  |  [CVE-2019-8451](https://lmgtfy.com/?q=CVE-2019-8451)  |
 
+## Reconnaissance
+
+If unauthenticated, one can access Confluence's landing page and retrieve version information from these places:
+
+- Login page footer.
+
+![](https://confluence.atlassian.com/confkb/files/980460833/980460769/1/1574450271730/Screen+Shot+2019-11-22+at+14.25.48.png)
+
+- Response Head AJS Tags.
+
+![](https://confluence.atlassian.com/confkb/files/980460833/980460798/1/1574450271658/Screen+Shot+2019-11-22+at+15.07.51.png)
+
+- Response Body What's New Link.
+
+![](https://confluence.atlassian.com/confkb/files/980460833/980460799/1/1574450271373/Screen+Shot+2019-11-22+at+15.10.01.png)
+
+- Response Body Confluence Help Link.
+
+![](https://confluence.atlassian.com/confkb/files/980460833/982321522/1/1576094162892/Screen+Shot+2019-12-11+at+16.55.54.png)
--- jiraffe/recon.py
+++ jiraffe/recon.py
@@ -4,14 +4,8 @@
 import requests
 import re
 import bs4
-
-"""
-One can access Confluence's landing page and retrieve version information from three places: [Unauthenticated]
-- Login page footer.
-- Response Head AJS Tags.
-- Response Body What's New Link.
-- Response Body Confluence Help Link.
-"""
+from urllib.parse import urlparse
+from packaging import version
 
 def request(target):
 	UA = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1"
@@ -31,6 +25,8 @@ def request(target):
 	return(r.text)
 
 def isjira(target): # reckless check but ok
+	url = urlparse(target)
+	target = url.scheme + "://" + url.netloc
 	response = request(target)
 	if "jira" in str(response):
 		return True
@@ -38,11 +34,31 @@ def isjira(target): # reckless check but ok
 		return False
 
 def getversion(target): # Jira version appears to be ____
-	target = target if "/login.jsp" in target else target + '/login.jsp'
+	url = urlparse(target)
+	target = url.scheme + "://" + url.netloc
+	# ENUM TYPE 1
+	f_build = '0.0.0' # default
+	vers = []
+	final_version = ""
+	target = target + url.path if "/login.jsp" in url.path else target + '/login.jsp'
 	response = request(target)
 	soup = bs4.BeautifulSoup(response, "html.parser")
+	
 	print("Checking for version ...")
 	try:
-		return soup.find("span", {"id": "footer-build-information"}).get_text() # Login page footer
+		vers = vers + [item["data-version"] for item in soup.find_all() if "data-version" in item.attrs] # ajs tags
+		f_build = soup.find("span", {"id": "footer-build-information"}).get_text() # Login page footer
+		vers.append(f_build.split("#")[0]) # grabbing & appending the version
 	except:
-		return False
+		pass
+	print(vers)
+	# finalize the version
+	try:
+		for n, i in enumerate(vers):
+			vers[n] = version.parse(i)
+		print(vers)
+		final_version = str(max(vers))
+	except Exception as e:
+		final_version = '0.0.0' # if fails
+
+	return final_version

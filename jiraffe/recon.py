4cf4529711f9333a69c6f91d2d07e8350a7824ca
Piyush Raj <hello@piyushraj.org>
2025-12-28 10:10:21 +0530

    refactor: remove dead code
    - Removed unreachable return paths in uparse()
    - Simplified isjira() detection logic
    - Make isaws() resilient to DNS resolution errors
    - Cleaned and clarified version parsing flow
    
    No functional or behavioral changes intended.

--- jiraffe/recon.py
+++ jiraffe/recon.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import requests, socket
-import re
+import sys
+import socket
+import requests
 import bs4
 from urllib.parse import urlparse
 from packaging import version
@@ -10,20 +11,25 @@ from packaging import version
 
 def uparse(target):
 	url = urlparse(target)
-	if url.path:
-		if url.path == "/":
-			print("[-] Target URL doesn't seems to be correct.\n\t\tValid Target URL Paths: http(s)://target.com/.../(login.action;/view.action;/viewpage.action;/releaseview.action;/aboutconfluencepage.action;/secure/Dashboard.jspa)")
-			return url.scheme + "://" + url.netloc + url.path
-		else:
-			return url.scheme + "://" + url.netloc + url.path
-		return 
-	else:
-		print("[-] Target URL doesn't seems to be correct.\n\t\tValid Target URL Paths: http(s)://target.com/.../(login.action;/view.action;/viewpage.action;/releaseview.action;/aboutconfluencepage.action;/secure/Dashboard.jspa)")
-		return url.scheme + "://" + url.netloc
+
+	if not url.scheme or not url.netloc:
+		print(
+			"[-] Target URL doesn't seem to be correct.\n\t\t"
+			"Valid Target URL Paths: http(s)://target.com/.../"
+			"(login.action;/view.action;/viewpage.action;/releaseview.action;"
+			"/aboutconfluencepage.action;/secure/Dashboard.jspa)"
+		)
+		return target
+
+	return url.scheme + "://" + url.netloc + (url.path or "")
 
 def request(target):
 	UA = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1"
-	headers = {'X-Atlassian-Token':'no-check', 'User-Agent':UA}
+	headers = {
+		"X-Atlassian-Token": "no-check",
+		"User-Agent": UA,
+	}
+
 	try:
 		r = requests.get(target, headers=headers)
 	except Exception as e:
@@ -32,45 +38,52 @@ def request(target):
 			print("[-] Something went wrong! (STATUS {})".format(r.status_code))
 			if r.status_code == 302:
 				print("[*] HTTP request got redirected. Set this instead: " + r.headers['Location'])
-		exit(1) # https://stackoverflow.com/a/2434619
+		sys.exit(1)
 
 	return r, r.text
 
 def isjira(target):
 	target = uparse(target)
-	res, response = request(target)
-	if "ajs-" in str(response):
-		if "footer-build-information" in str(response) or "atlassian-footer" in str(response):
-			return True
-	else:
-		return False
+	_, response = request(target)
+
+	return (
+		"ajs-" in response
+		and (
+			"footer-build-information" in response
+			or "atlassian-footer" in response
+		)
+	)
+
 
 def isaws(target):
-	target = urlparse(target)
-	data = socket.gethostbyaddr(target.netloc)
-	if "amazonaws" in str(data):
-		return True
-	else:
+	try:
+		host = urlparse(target).netloc
+		data = socket.gethostbyaddr(host)
+		return "amazonaws" in str(data)
+	except Exception:
 		return False
 
-def getversion(target): # ENUM #1: Jira version appears to be ____
+def getversion(target):
 	target = uparse(target)
-	f_build = '0.0.0' # default
-	vers = []
-	final_version = ""
-	res, response = request(target)
-	soup = bs4.BeautifulSoup(response, "html.parser")
-	try:
-		vers = vers + [item["data-version"] for item in soup.find_all() if "data-version" in item.attrs] # ajs tags
-		f_build = soup.find("span", {"id": "footer-build-information"}).get_text() # login page footer
-		vers.append(f_build.split("#")[0]) # grabbing & appending the version
-	except:
-		pass
-	try: # finalize the version
-		for n, i in enumerate(vers):
-			vers[n] = version.parse(i)
-		final_version = str(max(vers))
-	except Exception as e:
-		final_version = False # if fails
+	_, response = request(target)
 
-	return final_version
+	versions = []
+	soup = bs4.BeautifulSoup(response, "html.parser")
+
+	# ajs attributes
+	for tag in soup.find_all(attrs={"data-version": True}):
+		versions.append(tag["data-version"]) # ajs tags
+
+	# footer build info from login page
+	footer = soup.find("span", {"id": "footer-build-information"})
+	if footer:
+		versions.append(footer.get_text().split("#")[0])
+
+	if not versions:
+		return False
+
+	try:
+		parsed = [version.parse(v) for v in versions]
+		return str(max(parsed))
+	except Exception:
+		return False

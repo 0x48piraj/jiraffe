6bc181dd0a0d45219d742abb9f99e15dc8bbfb97
Piyush Raj <hello@piyushraj.org>
2025-12-28 10:47:49 +0530

    refactor: clean up CLI flow, exploit dispatch
    - Enforced Python 3 with explicit version guard
    - Unified styling
    - Removed platform-specific ANSI hacks
    - Centralized exploit registry and auto execution order
    - Simplified argument parsing and target validation
    - Clarified auto vs interactive execution paths
    
    No functional or user-facing behavior changes intended.

--- jiraffe/__main__.py
+++ jiraffe/__main__.py
@@ -1,116 +1,157 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os, sys, argparse, textwrap
-from .recon import request, isjira, getversion
-from .exploits import *
+import sys
+import argparse
+import textwrap
 
-def main():
-	if any(x in sys.platform.lower() for x in ['linux', 'linux2', 'win32']): # hotfix/issue#5
-	    os.system('color')
-	  # Group of Different functions for different styles
-	    class style():
-	        BLACK = lambda x: '\033[30m' + str(x)
-	        RED = lambda x: '\033[31m' + str(x)
-	        GREEN = lambda x: '\033[32m' + str(x)
-	        YELLOW = lambda x: '\033[33m' + str(x)
-	        BLUE = lambda x: '\033[34m' + str(x)
-	        MAGENTA = lambda x: '\033[35m' + str(x)
-	        CYAN = lambda x: '\033[36m' + str(x)
-	        WHITE = lambda x: '\033[37m' + str(x)
-	        UNDERLINE = lambda x: '\033[4m' + str(x)
-	        RESET = lambda x: '\033[0m' + str(x)
-	else:
-	    class style():
-	        BLACK = ""
-	        RED = ""
-	        GREEN = ""
-	        YELLOW = ""
-	        BLUE = ""
-	        MAGENTA = ""
-	        CYAN = ""
-	        WHITE = ""
-	        UNDERLINE = ""
-	        RESET = ""
+# version guard
+if sys.version_info[0] < 3:
+    sys.stderr.write("[-] Python 2 is not supported. Please use Python 3.\n")
+    sys.exit(1)
 
-	banner = textwrap.dedent(r'''
+from urllib.parse import urlparse
+from .recon import isjira, getversion
+from .exploits import (
+	cve2017_9506,
+	cve2019_8449,
+	cve2019_8451,
+	cve2019_11581,
+)
+
+
+class Style:
+	ENABLED = True
+
+	@staticmethod
+	def _wrap(code, text):
+		if not Style.ENABLED:
+			return str(text)
+		return "\033[{}m{}\033[0m".format(code, text)
+
+	BLACK = staticmethod(lambda x: Style._wrap("30", x))
+	RED = staticmethod(lambda x: Style._wrap("31", x))
+	GREEN = staticmethod(lambda x: Style._wrap("32", x))
+	YELLOW = staticmethod(lambda x: Style._wrap("33", x))
+	BLUE = staticmethod(lambda x: Style._wrap("34", x))
+	MAGENTA = staticmethod(lambda x: Style._wrap("35", x))
+	CYAN = staticmethod(lambda x: Style._wrap("36", x))
+	WHITE = staticmethod(lambda x: Style._wrap("37", x))
+	UNDERLINE = staticmethod(lambda x: Style._wrap("4", x))
+	RESET = staticmethod(lambda x: "\033[0m" + str(x))
+
+BANNER = textwrap.dedent(r'''
                                                                            /)/)
                                                                           ( ..\    
       ___  __      _______        __       _______   _______   _______    /'-._)
-     |"  ||" \    /"      \      /""\     /"     "| /"     "| /"     "|  /#/  
+     |   ||  \    /       \      /  \     /       | /       | /       |  /#/  
      ||  |||  |  |:        |    /    \   (: ______)(: ______)(: ______) /#/  @0x48piraj 
      |:  ||:  |  |_____/   )   /' /\  \   \/    |   \/    |   \/    |   
   ___|  / |.  |   //      /   //  __'  \  // ___)   // ___)   // ___)_  
- /  :|_/ )/\  |\ |:  __   \  /   /  \\  \(:  (     (:  (     (:      "| 
+ /  :|_/ )/\  |\ |:  __   \  /   /  \\  \(:  (     (:  (     (:       | 
 (_______/(__\_|_)|__|  \___)(___/    \___)\__/      \__/      \_______)
 ''')
 
-	parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=style.GREEN(banner) + style.RESET(''), usage=style.GREEN("jiraffe ") + style.YELLOW("[-h] [-t {}]").format(style.UNDERLINE("https://example-jira-instance.com") + style.RESET('') + style.YELLOW('')) + style.RESET(''))
-	optional = parser._action_groups.pop() # popped opt args
-	optional = parser.add_argument_group('Options')
-	optional.add_argument("-t", "--target", dest="target", metavar=style.CYAN("https://example-jira-instance.com") + style.RESET(''), default=False, help= style.GREEN("Target Jira Instance URL") + style.RESET(''))
-	optional.add_argument("-v", "--verbose", dest="verbose", action='store_true', help= style.GREEN("Verbose output") + style.RESET(''))
-	optional.add_argument("-a", "--auto", dest="automatic", action='store_true', help= style.GREEN("Automatic mode") + style.RESET(''))
+AUTO_EXPLOIT_ORDER = [
+    "3",  # CVE-2019-8451
+    "1",  # CVE-2017-9506
+    "2",  # CVE-2019-8449
+    "4",  # CVE-2019-11581
+]
 
-	verbose = parser.parse_args().verbose
-	target = parser.parse_args().target
-	auto = parser.parse_args().automatic
-	print(style.GREEN(banner) + style.RESET(''))
+INTERACTIVE_EXPLOITS = {
+	"1": ("CVE-2017-9506", cve2017_9506),
+	"2": ("CVE-2019-8449", cve2019_8449),
+	"3": ("CVE-2019-8451", cve2019_8451),
+	"4": ("CVE-2019-11581", cve2019_11581),
+}
+
+def validate_target(url):
+	parsed = urlparse(url)
+	return parsed.scheme in ("http", "https") and parsed.netloc
+
+def main():
+	parser = argparse.ArgumentParser(
+		formatter_class=argparse.RawDescriptionHelpFormatter,
+		description=Style.GREEN(BANNER),
+		usage=(
+			Style.GREEN("jiraffe ")
+			+ Style.YELLOW("[-h] [-t {}]").format(
+				Style.UNDERLINE("https://example-jira-instance.com")
+			)
+		),
+	)
+
+	parser.add_argument("-t", "--target", help="Target Jira URL")
+	parser.add_argument("-a", "--auto", action="store_true", help="Automatic mode")
+	parser.add_argument("-v", "--verbose", action="store_true", help="Verbose output")
+
+	args = parser.parse_args()
+
+	print(Style.GREEN(BANNER) + Style.RESET(""))
+
+	target = args.target
 	try:
-	    if target == False:
-	        print(style.YELLOW("[*] Target not provided, invoking interactive mode ..."))
-	        print("[*] Enter the target Jira instance URL (https://example-jira-instance.com)" + style.RESET(''))
-	        target = input(style.GREEN("    ----> ") + style.RESET('')).strip()
+		if not target:
+			print(Style.YELLOW("[*] Target not provided, invoking interactive mode..."))
+			print("[*] Enter the target Jira instance URL (https://example-jira-instance.com)")
+			target = input(Style.GREEN("	----> ")).strip()
 	except KeyboardInterrupt:
-	    print(style.RED('Interrupted.') + style.RESET(''))
-	    sys.exit(0) # http://tldp.org/LDP/abs/html/exitcodes.html#EXITCODESREF
+		print(Style.RED("Interrupted."))
+		sys.exit(0) # http://tldp.org/LDP/abs/html/exitcodes.html#EXITCODESREF
 
-	if target == "":
-		print(style.RED("[-] Not provided any target. Quitting.") + style.RESET(''))
+	if not target or not validate_target(target):
+		print(Style.RED("[-] Not provided any valid target. Quitting."))
 		sys.exit(1)
 
-	if auto == True:
-		print(style.YELLOW("[*] Detecting the target ...") + style.RESET(''))
-		if isjira(target): # detect if jira
-			print(style.GREEN("[+] Jira instance detected") + style.RESET(''), style.YELLOW("[*] Enumerating the version ...") + style.RESET(''), sep="\n")
-			vs = getversion(target) # grab version
-			if vs:
-				print(style.GREEN("[+] Jira version detected: {}").format(style.UNDERLINE(vs) + style.RESET('')) + style.RESET(''))
-			else:
-				print(style.RED("[-] Jira version detection failed.") + style.RESET(''))
-			print(style.YELLOW("[*] Launching all attacks ...") + style.RESET(''))
-			# should launch based on version
-			cve2019_8451(target)
-			cve2017_9506(target)
-			cve2019_8449(target)
-			cve2019_11581(target)
-	else:
-			print(style.YELLOW("[*] Mode not provided, invoking interactive mode ..."))
-			print("[*] Choose the exploit ..." + style.RESET(''))
-			EXMSG = (
-				'\n\n'
-				'1. CVE-2017-9506 [HIGH]\n'
-				'2. CVE-2019-8449 [LOW]\n'
-				'3. CVE-2019-8451 [HIGH]\n'
-				'4. CVE-2019-11581 [CRITICAL]'
+	if args.auto:
+		print(Style.YELLOW("[*] Detecting the target..."))
+		if isjira(target):
+			print(
+				Style.GREEN("[+] Jira instance detected") + "\n"
+				+ Style.YELLOW("[*] Enumerating the version...")
 			)
-			print(style.GREEN(EXMSG) + style.RESET(''))
-			exploit = input(style.GREEN("    ----> ") + style.RESET('')).strip()
-			if exploit == '1': # func. mapping
-				cve2017_9506(target)
-			elif exploit == '2':
-				cve2019_8449(target)
-			elif exploit == '3':
-				cve2019_8451(target)
-			elif exploit == '4':
-				print("[*] Input the payload (spawning harmless 'calc.exe', or maybe bash -c '...')")
-				command = input("Enter the payload (default: calc.exe): ")
-				if command:
-					cve2019_11581(target, command)
-				else:
-					cve2019_11581(target)
+
+			version = getversion(target)
+			if version:
+				print(
+					Style.GREEN("[+] Jira version detected: ")
+					+ Style.UNDERLINE(version)
+				)
 			else:
-				print(style.RED("[-] Invalid option selected. Quitting.") + style.RESET(''))
+				print(Style.RED("[-] Jira version detection failed."))
+
+			print(Style.YELLOW("[*] Launching all exploits..."))
+			for key in AUTO_EXPLOIT_ORDER:
+				_, exploit = INTERACTIVE_EXPLOITS[key]
+				exploit(target)
+	else:
+		print(Style.YELLOW("[*] Mode not provided, invoking interactive mode..."))
+		print("[*] Choose the exploit...")
+
+		print(Style.GREEN(
+			"\n\n"
+			"1. CVE-2017-9506 [HIGH]\n"
+			"2. CVE-2019-8449 [LOW]\n"
+			"3. CVE-2019-8451 [HIGH]\n"
+			"4. CVE-2019-11581 [CRITICAL]"
+		))
+
+		choice = input(Style.GREEN("	----> ")).strip()
+		if choice not in INTERACTIVE_EXPLOITS:
+			print(Style.RED("[-] Invalid option selected. Quitting."))
+			return
+
+		_, exploit = INTERACTIVE_EXPLOITS[choice]
+		if choice == "4":
+			print("[*] Input the payload (spawning harmless 'calc.exe', or maybe bash -c '...')")
+			cmd = input("Enter the payload (default: calc.exe): ").strip()
+			if cmd:
+				cve2019_11581(target, cmd)
+			else:
+				cve2019_11581(target)
+		else:
+			exploit(target)
 
 if __name__ == "__main__":
-    main()
+	main()

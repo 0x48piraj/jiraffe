b21d4877177ddb37d05dd87cc0ed3b6632f5a233
Piyush Raj <hello@piyushraj.org>
2025-12-31 13:33:09 +0530

    refactor(tests): modernize recon unit tests

--- tests/test_recon.py
+++ tests/test_recon.py
@@ -1,45 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import unittest
-from jiraffe.recon import *
+from jiraffe.recon import uparse, getversion, isjira, isaws
+from jiraffe.http import HttpClient
 
 
-class TestUtils(unittest.TestCase):
-	def test_uparse(self):
-		"""
-		Test that uparse() can parse and return base url
-		"""
-		data = 'https://jira.atlassian.com/secure/Dashboard.jspa'
-		result = uparse(data)
-		self.assertEqual(result, 'https://jira.atlassian.com/secure/Dashboard.jspa')
+class TestReconUtilities(unittest.TestCase):
 
-	def test_request(self):
-		"""
-		Test that request() works as intended
-		"""
-		data = 'https://jira.atlassian.com/secure/Dashboard.jspa'
-		result = request(data)
-		self.assertEqual(len(result), 2)
+    @classmethod
+    def setUpClass(cls):
+        cls.client = HttpClient()
 
-	def test_getversion(self):
-		"""
-		Test that getversion() returns a str containing jira version
-		"""
-		data = 'https://jira.atlassian.com/secure/Dashboard.jspa'
-		result = getversion(data)
-		self.assertIsInstance(result, str)
+    def test_uparse_preserves_valid_url(self):
+        """
+        uparse() should return a normalized URL
+        without altering a valid Jira path.
+        """
+        data = "https://jira.atlassian.com/secure/Dashboard.jspa"
+        result = uparse(data)
+        self.assertEqual(result, data)
 
-class TestBools(unittest.TestCase):
-	def test_isjira(self):
-		"""
-		Test that isjira() can detect jira instance
-		"""
-		data = 'https://jira.atlassian.com/secure/Dashboard.jspa'
-		result = isjira(data)
-		self.assertTrue(result)
+    def test_getversion_returns_string_or_none(self):
+        """
+        getversion() should return a semantic version string
+        or None if version detection fails.
+        """
+        data = "https://jira.atlassian.com"
+        result = getversion(data, self.client)
+        self.assertTrue(result is None or isinstance(result, str))
 
-	def test_isaws(self):
-		"""
-		Test that isaws() can detect aws instance
-		"""
-		data = 'https://jira.atlassian.com/secure/Dashboard.jspa'
-		result = isaws(data)
-		self.assertTrue(result)
+    def test_isjira_detects_jira_instance(self):
+        """
+        isjira() should correctly identify a Jira instance.
+        """
+        data = "https://jira.atlassian.com"
+        result = isjira(data, self.client)
+        self.assertTrue(result)
+
+    def test_isaws_known_aws_hostname(self):
+        """
+        isaws() should detect known AWS EC2 hostnames.
+        """
+        data = "https://ec2-3-91-23-45.compute-1.amazonaws.com"
+        result = isaws(data)
+        self.assertTrue(result)
+
+    def test_isaws_returns_boolean(self):
+        """
+        isaws() should always return a boolean value.
+        """
+        result = isaws("https://example.com")
+        self.assertIsInstance(result, bool)

dd98da9fa31e4f056ba24294f7666599980d133f
Piyush Raj <hello@piyushraj.org>
2026-01-03 16:52:46 +0530

    feat(recons): dev mode misconfiguration check

---
+++ jiraffe/recons/dev_mode.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from jiraffe.recons.base import ReconModule
+from jiraffe.enums import Severity
+
+
+class DevModeEnabled(ReconModule):
+    name = "Jira Dev Mode Enabled"
+    severity = Severity.LOW
+    description = "Jira running with dev mode enabled"
+
+    def run(self):
+        r = self.client.get(self.target)
+
+        if '<meta name="ajs-dev-mode" content="true">' in r.text:
+            print("[+] Jira dev mode enabled")
+            return True
+
+        return False

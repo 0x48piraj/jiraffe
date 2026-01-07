fcd581b8637328d9d0a08c52367b53d8cac60517
Piyush Raj <hello@piyushraj.org>
2026-01-07 20:46:06 +0530

    feat: optional no-reset flag to `Style._wrap` for inline color control

--- jiraffe/style.py
+++ jiraffe/style.py
@@ -5,19 +5,20 @@ class Style:
     ENABLED = True
 
     @staticmethod
-    def _wrap(code: str, text: str) -> str:
+    def _wrap(code: str, text: str, reset: bool = True) -> str:
+        """Wrap text with ANSI code, if reset=False, don't append reset code."""
         if not Style.ENABLED:
             return str(text)
-        return f"\033[{code}m{text}\033[0m"
+        return f"\033[{code}m{text}" + ("\033[0m" if reset else "")
 
-    BLACK = staticmethod(lambda x: Style._wrap("30", x))
-    RED = staticmethod(lambda x: Style._wrap("31", x))
-    GREEN = staticmethod(lambda x: Style._wrap("32", x))
-    YELLOW = staticmethod(lambda x: Style._wrap("33", x))
-    BLUE = staticmethod(lambda x: Style._wrap("34", x))
-    MAGENTA = staticmethod(lambda x: Style._wrap("35", x))
-    CYAN = staticmethod(lambda x: Style._wrap("36", x))
-    WHITE = staticmethod(lambda x: Style._wrap("37", x))
-    UNDERLINE = staticmethod(lambda x: Style._wrap("4", x))
-    RESET = staticmethod(lambda x: "\033[0m" + str(x))
-    ORANGE = staticmethod(lambda x: Style._wrap("38;5;208", x))
+    BLACK   = staticmethod(lambda x, reset=True: Style._wrap("30", x, reset))
+    RED     = staticmethod(lambda x, reset=True: Style._wrap("31", x, reset))
+    GREEN   = staticmethod(lambda x, reset=True: Style._wrap("32", x, reset))
+    YELLOW  = staticmethod(lambda x, reset=True: Style._wrap("33", x, reset))
+    BLUE    = staticmethod(lambda x, reset=True: Style._wrap("34", x, reset))
+    MAGENTA = staticmethod(lambda x, reset=True: Style._wrap("35", x, reset))
+    CYAN    = staticmethod(lambda x, reset=True: Style._wrap("36", x, reset))
+    WHITE   = staticmethod(lambda x, reset=True: Style._wrap("37", x, reset))
+    UNDERLINE = staticmethod(lambda x, reset=True: Style._wrap("4", x, reset))
+    RESET   = staticmethod(lambda x: "\033[0m" + str(x))
+    ORANGE  = staticmethod(lambda x, reset=True: Style._wrap("38;5;208", x, reset))
